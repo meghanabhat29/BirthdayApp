2a7c693dfe7a85f69d3b6b73c15dfe7859d9df02
meghanabhat29 <meghanaleo29@gmail.com>
2020-06-27 00:41:04 +0530

    Added an activity for the book.

---
+++ app/src/main/java/com/birthdayApp/Book.java
@@ -0,0 +1,51 @@
+package com.birthdayApp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.FileDescriptor;
+import java.io.IOException;
+
+public class Book extends AppCompatActivity {
+
+    private TextView textViewBook;
+    private ImageView imageViewBook;
+    public Button buttonBook;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book);
+
+        textViewBook = findViewById(R.id.book_text_view);
+        imageViewBook = findViewById(R.id.imageViewBook);
+        buttonBook = findViewById(R.id.buttonBook);
+        buttonBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openBook(view);
+            }
+        });
+    }
+
+
+    public void openURL(View view)
+    {
+        Intent intent = new Intent(this,URLClass.class);
+        startActivity(intent);
+    }
+}
+
+
+}
